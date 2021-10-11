413f2c7e39a66165b986ca468a501aaccb360573
joao vitor de lima gonçalves <88281625+joaolima16@users.noreply.github.com>
2021-10-11 14:28:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = window.document.getElementById("texto")
+    var image = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 14
+    msg.innerHTML =  `agora são ${hora} horas..`
+    if (hora>0 && hora <12){
+        Image.src ="imagens/imgmanha.jpg.jpg"
+        document.body.style.background= '#F0E68C'
+        
+}
+else if (hora>12 && hora<18){
+    image.src="imagens/imgtarde.jpg.jpeg"         
+    document.body.style.background= '#FF8C00'
+}
+
+else{
+    image.src="imagens/noite-256x256.png"
+    document.body.style.background ="#4169E1"
+}
+}
